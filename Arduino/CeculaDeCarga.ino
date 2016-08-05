3835566913ecc9fcfecdbc72eb393af0e7436419
RamonMA <ramonmonteiro_a@hotmail.com>
2016-08-05 10:06:08 -0300

    Create CeculaDeCarga.ino

---
+++ Arduino/CeculaDeCarga.ino
@@ -0,0 +1,17 @@
+float const RESOLUCAO = 0.0048828125;
+byte const SENSOR = A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int AD = analogRead(SENSOR);
+  float tensao = AD*RESOLUCAO;
+  Serial.print("AD: ");
+  Serial.print(AD);
+  Serial.print("tensao: ");
+  Serial.print(tensao);
+}
