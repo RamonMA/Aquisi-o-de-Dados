c1981adcfddd0fcf1b219401e5f440c33f964c0b
RamonMA <ramonmonteiro_a@hotmail.com>
2016-08-05 10:08:33 -0300

    Update CeculaDeCarga.ino

--- Arduino/CeculaDeCarga.ino
+++ Arduino/CeculaDeCarga.ino
@@ -2,12 +2,10 @@ float const RESOLUCAO = 0.0048828125;   //Tensão de Voltagem 5v e 10bits de res
 byte const SENSOR = A0;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int AD = analogRead(SENSOR);
   float tensao = AD*RESOLUCAO;
   Serial.print("AD: ");
